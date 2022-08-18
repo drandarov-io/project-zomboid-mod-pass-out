c58c0f0943d1389c5f35ff2fbb3e9abc33bfe312
Fajdek <100788476+Fajdek@users.noreply.github.com>
2022-08-18 19:41:22 +0200

    changed translation names lol

--- media/lua/shared/NPCs/PassOutMainCreationMethods.lua
+++ media/lua/shared/NPCs/PassOutMainCreationMethods.lua
@@ -3,8 +3,8 @@ local function PassOutTraits()
   -- Write number signifying how much points is spent/gained from a trait in the "cost" value
   -- Negative number gives you points, and positive number spends it
   -- False means the trait is *not* hidden and can be seen when creating a character, use true for occupation traits
-    TraitFactory.addTrait("ProneToPassing", getText("UI_trait_PassOutProne"), cost, getText("UI_trait_PassOutProneDesc"), false);
-    TraitFactory.addTrait("ResistantToPassing", getText("UI_trait_PassOutResistant"), cost, getText("UI_trait_PassOutResistantDesc"), false);
+    TraitFactory.addTrait("ProneToPassing", getText("UI_trait_ProneToPassing"), cost, getText("UI_trait_ProneToPassingDesc"), false);
+    TraitFactory.addTrait("ResistantToPassing", getText("UI_trait_ResistantToPassing"), cost, getText("UI_trait_ResistantToPassingDesc"), false);
     TraitFactory.setMutualExclusive("ProneToPassing", "ResistantToPassing");
 end
 
