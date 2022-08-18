ca625ed41d7cbc1d39459c3fad8cbc997c316129
Fajdek <100788476+Fajdek@users.noreply.github.com>
2022-08-18 19:44:46 +0200

    added a check for if server has disabled sleeping

--- media/lua/shared/NPCs/PassOutMainCreationMethods.lua
+++ media/lua/shared/NPCs/PassOutMainCreationMethods.lua
@@ -3,8 +3,9 @@ local function PassOutTraits()
   -- Write number signifying how much points is spent/gained from a trait in the "cost" value
   -- Negative number gives you points, and positive number spends it
   -- False means the trait is *not* hidden and can be seen when creating a character, use true for occupation traits
-    TraitFactory.addTrait("ProneToPassing", getText("UI_trait_ProneToPassing"), cost, getText("UI_trait_ProneToPassingDesc"), false);
-    TraitFactory.addTrait("ResistantToPassing", getText("UI_trait_ResistantToPassing"), cost, getText("UI_trait_ResistantToPassingDesc"), false);
+  local sleepOK = (isClient() or isServer()) and getServerOptions():getBoolean("SleepAllowed") and getServerOptions():getBoolean("SleepNeeded")
+    TraitFactory.addTrait("ProneToPassing", getText("UI_trait_ProneToPassing"), cost, getText("UI_trait_ProneToPassingDesc"), false, not SleepOK);
+    TraitFactory.addTrait("ResistantToPassing", getText("UI_trait_ResistantToPassing"), cost, getText("UI_trait_ResistantToPassingDesc"), false, not SleepOK);
     TraitFactory.setMutualExclusive("ProneToPassing", "ResistantToPassing");
 end
 
